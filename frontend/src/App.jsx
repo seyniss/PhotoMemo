68e435bc28b0e7e84511ae9042d5304c6fa6da34
seyniss <shepunk2@kbu.ac.kr>
2025-09-30 21:20:36 +0900

    /logout, frontend setting
    로그아웃 라우팅+로그인 실패 횟수 제한
    그리고 프론트 기초 세팅?

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+import Landing from "./pages/Landing"
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <Routes>
+     <Route path="/" element={<Landing />}/>
+
+    </Routes>
+  )
+}
+
+export default App
